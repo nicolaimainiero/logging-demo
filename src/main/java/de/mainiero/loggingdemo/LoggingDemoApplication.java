e1c36f7c764ac7b526a00ee09a08ee3b37d6c2f7
Nicolai Mainiero <nicolai@mainiero.de>
2021-10-18 11:28:25 +0200

    Add random stacktraces to the logging output

--- src/main/java/de/mainiero/loggingdemo/LoggingDemoApplication.java
+++ src/main/java/de/mainiero/loggingdemo/LoggingDemoApplication.java
@@ -15,16 +15,19 @@ import java.util.Date;
 @SpringBootApplication
 public class LoggingDemoApplication {
 
-	private static final DateFormat DATE_FORMAT = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.LONG);
+    private static final DateFormat DATE_FORMAT = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.LONG);
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoggingDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingDemoApplication.class, args);
+    }
 
-	@Scheduled(initialDelay = 0, fixedRate = 1000)
-	public void log(){
-		String currentDate = DATE_FORMAT.format(new Date());
-		log.log(Level.INFO, "This is a repeating log message: {}", currentDate);
-	}
+    @Scheduled(initialDelay = 0, fixedRate = 1000)
+    public void log() throws Exception {
+        String currentDate = DATE_FORMAT.format(new Date());
+        log.log(Level.INFO, "This is a repeating log message: {}", currentDate);
+        if (0.9 < Math.random()) {
+            throw new Exception("This should print a stacktrace");
+        }
+    }
 
 }
